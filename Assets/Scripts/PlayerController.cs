ddd802458f883e6f5c89717b91e249c5fcafb9d8
FrucoAndres <salsitarpremorg121@gmail.com>
2025-09-30 18:44:29 -0500

    Primer commit del rolaball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float speed;
+
+    public Transform particulas;
+
+    public Text textoContador;
+
+    private int contador;
+
+    private ParticleSystem sistemaParticulas;
+
+    private Vector3 posicion;
+
+    private Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movimiento * speed);
+
+    }
+
+    void Start()
+    {
+        sistemaParticulas = particulas.GetComponent<ParticleSystem>();
+
+        sistemaParticulas.Stop();
+
+        rb = GetComponent<Rigidbody>();   
+
+        textoContador.text = "Contador: " + contador.ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Recolectable")){
+            other.gameObject.SetActive(false);
+            contador = contador + 1;
+            textoContador.text = "Contador: " + contador.ToString();
+        } else {
+
+            //No es recolectable
+
+        }
+    }
+}
